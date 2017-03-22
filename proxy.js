16d37919501aca3d0db20adf075a9a60ad85b473
Youchen Lee <copyleft@utcr.org>
2017-03-23 01:23:24 +0800

    decode login info & use cookie-session to store it.

--- proxy.js
+++ proxy.js
@@ -2,48 +2,85 @@
 
 const express = require('express');
 const crypto = require('crypto');
+const url = require('url');
+const querystring = require('querystring');
+const cookieSession = require('cookie-session');
+const config = require('./config');
 
-var config = require('./config');
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV);
 }
 
 // Discourse SSO
 const DISCOURSE_SSO_SECRET = process.env.DISCOURSE_SSO_SECRET;
-const DISCOURSE_HOST = process.env.DISCOURSE_HOST; // https://talk.pdis.nat.gov.tw
+const DISCOURSE_HOST = process.env.DISCOURSE_HOST || "https://talk.pdis.nat.gov.tw";
+const COOKIE_KEY = process.env.COOKIE_KEY || "dev";
+const COOKIE_KEY2 = process.env.COOKIE_KEY || process.env.DISCOURSE_SSO_SECRET; // in case key rotation
 const proxy_port = process.env.PROXY_PORT || config.dev.proxy_port;
 
 
 const app = express();
 
+app.use(cookieSession({
+  name: 'session',
+  keys: [COOKIE_KEY, COOKIE_KEY2],
+  // Cookie Options
+  maxAge: 1 * 60 * 60 * 1000 // 1 hours (for dev), TODO change to 24 hours later
+}));
+
+
+app.use(function (req, res, next) {
+  // expand cookie expire time every minute
+  req.session.nowInMinutes = Math.floor(Date.now() / 60e3);
+  next();
+});
+
 app.get('/login', (req, res) => {
-  var current_host = req.protocol + '://' + req.get('host');
+  let returnUrl = `${req.protocol}://${req.get('host')}/sso_done`;
 
   // source: https://github.com/edhemphill/passport-discourse/blob/master/lib/discourse-sso.js
-  var ret = {};
-  var hmac = crypto.createHmac('sha256', DISCOURSE_SSO_SECRET);
+  let hmac = crypto.createHmac('sha256', DISCOURSE_SSO_SECRET);
 
   crypto.randomBytes(16, (err, buf) => {
     if (err) throw err;
 
-    ret._created_at = new Date();
-    ret.nonce = buf.toString('hex');
-    var payload = "nonce=" + ret.nonce + "&return_sso_url=" + current_host + '/sso_done';
-    var payload_b64 = new Buffer(payload).toString('base64');
-    hmac.update(payload_b64);
-    ret.hex_sig = hmac.digest('hex');
-    ret.urlenc_payload_b64 = encodeURIComponent(payload_b64);
-    ret.url_redirect = DISCOURSE_HOST +"/session/sso_provider?sso=" + ret.urlenc_payload_b64 + "&sig=" + ret.hex_sig;
+    let nonce = buf.toString('hex');
+    let payload = new Buffer(`nonce=${nonce}&return_sso_url=${returnUrl}`).toString('base64');
+    let sig = hmac.update(payload).digest('hex');
+    let urlRedirect = `${DISCOURSE_HOST}/session/sso_provider?sso=${encodeURIComponent(payload)}&sig=${sig}`;
 
-    res.redirect(ret.url_redirect);
+    res.redirect(urlRedirect);
   });
 });
 
 app.get('/sso_done', (req, res) => {
-  console.log(req);
   // TODO get user profile.
-  res.send('discourse sso done');
+  let sso = req.query.sso;
+  let sig = req.query.sig;
+
+  let hmac = crypto.createHmac('sha256', DISCOURSE_SSO_SECRET);
+  let decodedSso = decodeURIComponent(sso);
+  let hash = hmac.update(decodedSso).digest('hex');
+
+  if (sig != hash) {
+    console.error('Invalid auth');
+    res.status(403).send('Invalid auth');
+  }
+
+  let profile = querystring.parse(new Buffer(sso, 'base64').toString('utf8'));
+
+  // TODO check that profile.nonce should match the nonce from the login step.
+  console.log(profile);
+
+  req.session.username = profile.username;
+  res.end('Hello ' + profile.username + ', cookie-session saved! Check it at /whoami');
 });
+
+app.get('/whoami', (req, res) => {
+  let username = req.session.username;
+  res.end('You are ' + username);
+});
+
 
 app.listen(proxy_port, () => {
   console.log(`server started at localhost:${proxy_port}`);
