ae381f8839858f95a0871acce3dfbd1bdd66dfe3
mark19891107 <mark19891107@gmail.com>
2017-03-22 18:49:59 +0800

    header init

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template lang="pug">
   #app
+    Header
     router-view
 </template>
 
 <script>
+  import Header from './components/Header.vue'
+
   export default {
-    name: 'app'
+    name: 'app',
+    components: {
+      Header
+    }
   }
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  .header
+    
+</template>
+
+<script>
+  export default {
+    name: 'header',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
