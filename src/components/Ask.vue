5b9272cdbf6127956b9834cfed2d8c16860a9d85
FreedomKnight <fk@unisharp.com>
2017-03-23 00:33:11 +0800

    add simple ask component

---
+++ src/components/Ask.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  el-form
+    el-form-item
+      el-input(type='textarea' v-model="question")
+    el-form-item
+      el-button(type="primary", @click="submit") 提問
+    
+</template>
+
+<script>
+  export default {
+    name: 'ask',
+    data () {
+      return {
+        question: ''
+      }
+    },
+    methods: {
+      submit: function () {
+        console.log(this.question)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
