b3997d5afe7670b5b77156576fac3c272c76e269
smith02620 <swith02620@ey.gov.tw>
2017-03-24 15:54:32 +0800

    add time title icon aouther content

--- src/components/Wisdom.vue
+++ src/components/Wisdom.vue
@@ -4,16 +4,26 @@
       el-col(:span='4')
         p 
       el-col(:span='16')
+       div(v-for='(item, contentindex) in wisdom.content')
         el-card.box-card
           .clearfix(slot='header')
             span(style='line-height: 36px;')
-            p {{wisdom.title[0]}}
-            p {{wisdom.content[0][0]}}
+            img(:src='wisdom.icon[contentindex][0]')
+            span.el-dialog__title {{wisdom.aouther[contentindex][0]}}
+            span 提問:
+            h2 {{wisdom.title[contentindex]}}
+            p(v-html='wisdom.content[contentindex][0]')
             
-          .text.item(v-for='(item, index) in wisdom.content[0]')
-            p(v-if='index!=0') {{wisdom.content[0][index]}}
+          .text.item(v-for='(item, index) in wisdom.content[contentindex]',v-if='index!=0',v-bind:class="{sereply: index>=2}")
+              
+              img(:src='wisdom.icon[contentindex][index]')
+              span.el-dialog__title {{wisdom.aouther[contentindex][index]}}
+              span 回應:
+              span.sereply(v-html='wisdom.content[contentindex][index]')
+
+          el-input.sereply(type='textarea', autosize='', placeholder='我要回應...', v-model='textarea2')
       el-col(:span='4')
-        P 456
+        P 
 
     
 </template>
@@ -29,7 +39,9 @@
         wisdom: {
           title: [],
           icon: [],
-          content: []
+          content: [],
+          aouther: [],
+          time: []
         }
       }
     },
@@ -45,18 +57,29 @@
               this.wisdom.title.push(topic['data']['title'])
               let content = []
               let icon = []
+              let aouther = []
+              let time = []
               for (let j in topic['data']['post_stream']['posts']) {
-                content.push(topic['data']['post_stream']['posts'][j]['raw'])
+                content.push(topic['data']['post_stream']['posts'][j]['cooked'])
+                aouther.push(topic['data']['post_stream']['posts'][j]['name'])
+                time.push(topic['data']['post_stream']['posts'][j]['created_at'].replace(/T.*/, ''))
                 if (topic['data']['post_stream']['posts'][j]['avatar_template'].indexOf('https:') === -1) {
                   icon.push('https://talk.pdis.nat.gov.tw' + topic['data']['post_stream']['posts'][j]['avatar_template'].replace(/{size}/, '100'))
                 }
               }
               this.wisdom.content.push(content)
               this.wisdom.icon.push(icon)
+              this.wisdom.aouther.push(aouther)
+              this.wisdom.time.push(time)
             })
           }
           console.log(this.wisdom)
         })
+      }
+    },
+    watch: {
+      wisdom: function () {
+        this.getDiscussion_Comment()
       }
     },
     created: function () {
@@ -67,7 +90,7 @@
 
 <style lang="scss" scoped>
   .wisdom {
-    text-align: center;
+    // text-align: center;
 
     .avatar {
       border-radius: 50%;
@@ -87,12 +110,42 @@
   .clearfix:after {
       display: table;
       content: "";
+      
   }
   .clearfix:after {
       clear: both
   }
 
   .box-card {
+    line-height: 2em;
     width: 100%;
+    margin-bottom: 2em;
+    img {
+        width: 5%;
+        vertical-align: middle;
+        margin-right: 1em;
+      }
+    .el-dialog__title{
+      margin-right: 1em;
+    }
+    .sereply{
+      margin-left:3em;
+    }
+    .el-dialog__title{
+      font-size:1.5rem;
+    }
+    el-input{
+      width:50%;
+    }
+    .el-textarea {
+      display: inline-block;
+      width: 88%;
+      vertical-align: bottom;
+    }
   }
+  .el-card__body {
+    padding: 20px;
+    background-color: rgba(11, 0, 236, 0.08) !important;
+}
+    
 </style>
