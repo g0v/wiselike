5fd16833e59ee7b590b84165f23093ae9680fddf
john5601 <john560148@gmail.com>
2017-03-30 16:30:22 +0800

    拿掉vue預設畫面

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,30 +1,6 @@
 <template lang="pug">
   .hello
-    img(src="../assets/logo.png")
-    h1 {{ msg }}
-    h2 Essential Links
-    ul
-      li
-        a(href="https://vuejs.org", target="_blank") Core Docs
-      li
-        a(href="https://forum.vuejs.org", target="_blank") Forum
-      li
-        a(href="https://gitter.im/vuejs/vue", target="_blank") Gitter Chat
-      li
-        a(href="https://twitter.com/vuejs", target="_blank") Twitter
-      br
-      li
-        a(href="http://vuejs-templates.github.io/webpack/", target="_blank") Docs for This Template
-    h2 Ecosystem
-    ul
-      li
-        a(href="http://router.vuejs.org/", target="_blank") vue-router
-      li
-        a(href="http://vuex.vuejs.org/", target="_blank") vuex
-      li
-        a(href="http://vue-loader.vuejs.org/", target="_blank") vue-loader
-      li
-        a(href="https://github.com/vuejs/awesome-vue", target="_blank") awesome-vue
+    h2 {{msg}}
     wisdom
 </template>
 
@@ -37,28 +13,14 @@
     },
     data () {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        msg: 'Welcome to Wiselike!'
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
+h2{
+  text-align: center;
+}
 </style>
