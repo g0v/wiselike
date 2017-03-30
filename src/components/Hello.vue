50904edda493aa4c567183340f2ec3f183f8a3af
smith02620 <swith02620@ey.gov.tw>
2017-03-30 15:21:20 +0800

    放上profile

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,39 +1,17 @@
 <template lang="pug">
   .hello
-    img(src="../assets/logo.png")
-    h1 {{ msg }}
-    h2 Essential Links
-    ul
-      li
-        a(href="https://vuejs.org", target="_blank") Core Docs
-      li
-        a(href="https://forum.vuejs.org", target="_blank") Forum
-      li
-        a(href="https://gitter.im/vuejs/vue", target="_blank") Gitter Chat
-      li
-        a(href="https://twitter.com/vuejs", target="_blank") Twitter
-      br
-      li
-        a(href="http://vuejs-templates.github.io/webpack/", target="_blank") Docs for This Template
-    h2 Ecosystem
-    ul
-      li
-        a(href="http://router.vuejs.org/", target="_blank") vue-router
-      li
-        a(href="http://vuex.vuejs.org/", target="_blank") vuex
-      li
-        a(href="http://vue-loader.vuejs.org/", target="_blank") vue-loader
-      li
-        a(href="https://github.com/vuejs/awesome-vue", target="_blank") awesome-vue
+    profile
     wisdom
 </template>
 
 <script>
+  import profile from './Profile.vue'
   import wisdom from './Wisdom.vue'
   export default {
     name: 'hello',
     components: {
-      wisdom
+      wisdom,
+      profile
     },
     data () {
       return {
