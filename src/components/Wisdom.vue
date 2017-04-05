d97a6f62813d7b1838c1075769413082a7026906
smith02620 <swith02620@ey.gov.tw>
2017-04-05 17:25:39 +0800

    fixed wisdom

--- src/components/Wisdom.vue
+++ src/components/Wisdom.vue
@@ -46,23 +46,40 @@
         },
         page: Number,
         lazyload: '',
+        lazyload_count: '',
         All_category: []
       }
     },
     methods: {
       getUserData: async function () {
         // this.getDiscussion_Comment('https://talk.pdis.nat.gov.tw/c/wiselike/profile-audreyt/l/latest.json?page=0')
-        this.lazyload = 2
+        this.lazyload = 0
+        this.lazyload_count = 0
         this.page = 0
         this.All_category = await this.getDiscussion_Category('https://talk.pdis.nat.gov.tw/c/wiselike/profile-audreyt/l/latest.json?page=0')
-        let topic = await this.getDiscussion_Topic(this.All_category)
-        await this.Data_Processing(topic)
+        await this.lazy()
       },
       lazy: async function (val) { // lazyload
-        this.lazyload += 2
-        let topic = await this.getDiscussion_Topic(this.All_category)
+        let topic = []
+        let length = this.All_category.data.topic_list.topics.length
+        if (length - this.lazyload_count === 1) {
+          this.lazyload_count += 1
+          this.lazyload += 1
+          for (let i = (this.lazyload_count - 1); i < this.lazyload_count; i++) {
+            let topicdata = await this.getDiscussion_Topic(this.All_category, i)
+            topic.push(topicdata)
+          }
+        } else if (length - this.lazyload_count > 1) {
+          this.lazyload_count += 2
+          this.lazyload += 2
+          for (let i = (this.lazyload_count - 2); i < this.lazyload_count; i++) {
+            let topicdata = await this.getDiscussion_Topic(this.All_category, i)
+            topic.push(topicdata)
+          }
+        }
         await this.Data_Processing(topic)
-        if ((this.lazyload % 30) === 0) {
+        if (this.lazyload_count === length) {
+          this.lazyload_count = 0
           this.page += 1
           this.All_category = await this.getDiscussion_Category('https://talk.pdis.nat.gov.tw/c/wiselike/profile-audreyt/l/latest.json?page=' + this.page)
         }
@@ -70,47 +87,23 @@
       getDiscussion_Category: function (url) { // 抓取作者全部的category
         return new Promise((resolve, reject) => {
           axios.get(url).then((val) => {
-            if (url.indexOf('page=0') > -1) {
-              val['data']['topic_list']['topics'] = val['data']['topic_list']['topics'].slice(1)
-              resolve(val)
-            } else {
-              resolve(val)
-            }
+            val['data']['topic_list']['topics'] = val['data']['topic_list']['topics'].slice(1)
+            resolve(val)
+            // if (url.indexOf('page=0') > -1) {
+            //   val['data']['topic_list']['topics'] = val['data']['topic_list']['topics'].slice(1)
+            //   resolve(val)
+            // } else {
+            //   resolve(val)
+            // }
           })
         })
       },
-      getDiscussion_Topic: function (url) { // 一次抓兩篇topic
+      getDiscussion_Topic: async function (url, i) { // 抓topic
         return new Promise((resolve, reject) => {
           let id = url['data']['topic_list']['topics']
-          let data = []
-          // function a (i, callback) {
-          //   let con = i + 2
-          //   while (i < con) {
-          //     axios.get('https://talk.pdis.nat.gov.tw/t/' + id[i % 10].id + '.json?include_raw=1').then((val) => {
-          //       data.push(val)
-          //       console.log(id[1].id)
-          //       if (data.length === 2) {
-          //         resolve(data)
-          //         console.log(data)
-          //       }
-          //       if (data.length === 1) {
-          //         i++
-          //       }
-          //     })
-          //     i++
-          //   }
-          //   callback(i)
-          // }
-          // a((this.lazyload - 2), function (result) {})
-          for (let i = (this.lazyload - 2); i < this.lazyload; i++) {
-          // for (let i in id) {
-            axios.get('https://talk.pdis.nat.gov.tw/t/' + id[i % 10].id + '.json?include_raw=1').then((val) => {
-              data.push(val)
-              if (data.length === 2) {
-                resolve(data)
-              }
-            })
-          }
+          axios.get('https://talk.pdis.nat.gov.tw/t/' + id[i].id + '.json?include_raw=1').then((val) => {
+            resolve(val)
+          })
         })
       },
       Data_Processing: function (topic) {
@@ -133,42 +126,6 @@
           this.wisdom.time.push(time)
           this.wisdom.icon.push(icon)
         }
-      },
-      getDiscussion_Comment: function (url) {
-        return new Promise((resolve, reject) => {
-          axios.get(url)
-          .then((val) => {
-            val['data']['topic_list']['topics'] = val['data']['topic_list']['topics'].slice(1)
-            for (let i in val.data.topic_list.topics) {
-              axios.get('https://talk.pdis.nat.gov.tw/t/' + val.data.topic_list.topics[i].id + '.json?include_raw=1')
-              .then((topic) => {
-              //   this.wisdom.title.push(i)
-                this.wisdom.title.push(topic['data']['title'])
-                let content = []
-                let icon = []
-                let aouther = []
-                let time = []
-                for (let j in topic['data']['post_stream']['posts']) {
-                  content.push(topic['data']['post_stream']['posts'][j]['cooked'])
-                  aouther.push(topic['data']['post_stream']['posts'][j]['name'])
-                  time.push(topic['data']['post_stream']['posts'][j]['created_at'].replace(/T.*/, ''))
-                  if (topic['data']['post_stream']['posts'][j]['avatar_template'].indexOf('https:') === -1) {
-                    icon.push('https://talk.pdis.nat.gov.tw' + topic['data']['post_stream']['posts'][j]['avatar_template'].replace(/{size}/, '100'))
-                  }
-                }
-                this.wisdom.content.push(content)
-                this.wisdom.icon.push(icon)
-                this.wisdom.aouther.push(aouther)
-                this.wisdom.time.push(time)
-                if (this.wisdom.content.length === val.data.topic_list.topics.length) {
-                  this.lazyload = 2
-                  resolve(this.wisdom)
-                }
-                // this.lazyload(this.wisdom)
-              })
-            }
-          })
-        })
       }
     },
     created: function () {
