2d673ccfd3c33943368a75d593255648c728fe3f
smith02620 <swith02620@ey.gov.tw>
2017-05-23 11:59:27 +0800

    fix: proxy(axios header)

--- proxy.js
+++ proxy.js
@@ -62,6 +62,12 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
+app.all('/*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.get('/login', (req, res) => {
   let returnUrl = `${req.protocol}://${req.get('host')}/sso_done`
   // source: https://github.com/edhemphill/passport-discourse/blob/master/lib/discourse-sso.js
