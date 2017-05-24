868ac2b140be9cc10da5e3c2db268cb2d8282421
smith02620 <swith02620@ey.gov.tw>
2017-05-24 17:55:56 +0800

    fix:App(wiselike topic < 10)

--- src/App.vue
+++ src/App.vue
@@ -97,7 +97,7 @@
             })
             /* drop first topic which is actually meta */
             newTopics = newTopics.slice(1)
-            this.topicList = newTopics
+            this.topicList = newTopics.slice(0, 10)
             newTopics = newTopics.slice(0, 10)
             /* find the profile owner by category id from each topic */
             return Promise.all(newTopics.map((topic) => axios.get('https://talk.pdis.nat.gov.tw/c/wiselike/' + topic.category + '.json')))
