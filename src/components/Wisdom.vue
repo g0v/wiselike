51492c70434ba4139e9ebaccce97e1a45e71cfc3
smith02620 <swith02620@ey.gov.tw>
2017-06-03 17:13:26 +0800

    fix:header

--- src/components/Wisdom.vue
+++ src/components/Wisdom.vue
@@ -26,7 +26,6 @@
       .content(v-html='post.content')
     
     div.replyButton(v-if="!reply && local_storage.username !== undefined")
-      img.avatar(:src='local_storage.userIcon')
       el-button(type='primary', @click="reply = true") 我 要 回 覆
     div.center(v-else-if="local_storage.username === undefined")
       el-button(@click.native="login",type="warning") 請 先 登 入 方 可 留 言
