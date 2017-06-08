14fadc885a5f45176932e1a3158ad5b40e8c31f0
smith02620 <swith02620@ey.gov.tw>
2017-06-08 22:17:14 +0800

    change: index(router lazy load)

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-import Me from '@/components/Me'
-import Profile from '../components/Profile.vue'
-import Searchresult from '../components/Searchresult.vue'
+// import Hello from '@/components/Hello'
+// import Me from '@/components/Me'
+// import Profile from '../components/Profile.vue'
+// import Searchresult from '../components/Searchresult.vue'
 
 Vue.use(Router)
 
@@ -12,22 +12,26 @@ export default new Router({
     {
       path: '/',
       name: 'Hello',
-      component: Hello
+      // component: Hello，
+      component: resolve => require(['@/components/Hello'], resolve)
     },
     {
       path: '/me',
       name: 'Me',
-      component: Me
+      // component: Me
+      component: resolve => require(['@/components/Me'], resolve)
     },
     {
       path: '/user/:userId',
       name: 'profile',
-      component: Profile
+      // component: Profile
+      component: resolve => require(['../components/Profile.vue'], resolve)
     },
     {
       path: '/Searchresult/:category/:result',
       name: 'Search_result',
-      component: Searchresult
+      // component: Searchresult
+      component: resolve => require(['../components/Searchresult.vue'], resolve)
     }
   ]
 })
