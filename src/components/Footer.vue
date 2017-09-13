322c257350e9262a458a574bb3fcbf32b93a6ab7
john5601 <john560148@gmail.com>
2017-09-13 10:45:40 +0800

    Add more statement of TOS in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,6 +12,7 @@
           | 回報建議
     el-col.license(:lg='16', :sm='24')
       p This license lets others distribute, remix, tweak, and build upon your work, even commercially, as long as they credit you for the original creation. This is the most accommodating of licenses offered. Recommended for maximum dissemination and use of licensed materials.
+      p 我們深信網路言論自由就是一切，本網站不會進行任何形式的內容審查；除非應法院或其他政府機關依法提出的要求，本網站也不會主動移除他人在上面發表的內容。
 </template>
 
 <script>
@@ -44,6 +45,7 @@
     flex-flow: row wrap;
     align-items: center;
     justify-content: center;
+    padding:1em;
     .logo {
       margin: 1ch;
       i {
@@ -60,6 +62,7 @@
     padding: 1em;
     font-size: 0.8rem;
     color: gray;
+    font-weight:800;
   }
 }
 </style>
